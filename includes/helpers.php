704a80e74885acf39846420899e654dddaeb07af
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-11-16 21:39:50 -0500

    wrapResults alternate

--- includes/helpers.php
+++ includes/helpers.php
@@ -9,26 +9,28 @@ function runQuery($sql, $params = []) {
 }
 
 function wrapResultsInTable($stmt) {
-    if ($stmt->rowCount()=== 0) {
-        return "<p>No results found.</p>";
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($rows) === 0) {
+        return "<table class='results'><tr><td>No results found.</td></tr></table>";
     }
 
-    $html = "<table border ='1'><tr>";
-    $columns = array_keys($stmt->fetch(PDO::FETCH_ASSOC));
+    $html = "<table class='results'><tr>";
+    $columns = array_keys($rows[0]);
     foreach ($columns as $col) {
         $html .= "<th>" . htmlspecialchars($col) . "</th>";
     }
     $html .= "</tr>";
 
-    $stmt->execute();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    foreach ($rows as $row) {
         $html .= "<tr>";
         foreach ($row as $value) {
             $html .= "<td>" . htmlspecialchars($value) . "</td>";
         }
         $html .= "</tr>";
     }
-    $html .= "</table";
+
+    $html .= "</table>";
     return $html;
 }
 ?>
